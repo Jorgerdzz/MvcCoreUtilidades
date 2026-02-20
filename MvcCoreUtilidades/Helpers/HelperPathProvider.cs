e8229e3389758b100083670e9777de9b8d46b737
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-02-20 09:35:52 +0100

    Provider subir ficheros realizado

---
+++ MvcCoreUtilidades/Helpers/HelperPathProvider.cs
@@ -0,0 +1,37 @@
+﻿namespace MvcCoreUtilidades.Helpers
+{
+    //Enumeracion con las carpetas que deseemos subir ficheros
+
+    public enum Folders { Uploads, Images, Facturas}
+
+    public class HelperPathProvider
+    {
+        private IWebHostEnvironment hostEnvironment;
+
+        public HelperPathProvider(IWebHostEnvironment hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        //TENDREMOS UN METODO QUE SE ENCARGARA DE RESOLVER LA RUTA
+        //COMO UN STRING CUANDO RECIBAMOS EL FICHERO Y LA CARPETA
+        public string MapPath(string fileName, Folders folder)
+        {
+            string carpeta = "";
+            if(folder == Folders.Images)
+            {
+                carpeta = "images";
+            }else if(folder == Folders.Uploads)
+            {
+                carpeta = "uploads";
+            }else if(folder == Folders.Facturas)
+            {
+                carpeta = "facturas";
+            }
+            string rootPath = this.hostEnvironment.WebRootPath;
+            string path = Path.Combine(rootPath, carpeta, fileName);
+            return path;
+        }
+
+    }
+}
