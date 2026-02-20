d1d56ae57ffd6c50ada8b6b1b45f27ea9a17c8e0
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-02-20 12:42:36 +0100

    Implementación Memoria personalizada

--- MvcCoreUtilidades/Controllers/CachingController.cs
+++ MvcCoreUtilidades/Controllers/CachingController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace MvcCoreUtilidades.Controllers
 {
     public class CachingController : Controller
     {
+
+        private IMemoryCache memoryCache;
+
+        public CachingController(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -14,6 +23,30 @@ namespace MvcCoreUtilidades.Controllers
         {
             string fecha = DateTime.Now.ToLongDateString() + " --- " + DateTime.Now.ToLongTimeString();
             ViewData["FECHA"] = fecha;
+            return View();
+        }
+
+        public IActionResult MemoriaPersonalizada(int? tiempo)
+        {
+            if(tiempo == null)
+            {
+                tiempo = 60;
+            }
+            string fecha = DateTime.Now.ToLongDateString() + " --- " + DateTime.Now.ToLongTimeString();
+            if (this.memoryCache.Get("FECHA") == null)
+            {
+                MemoryCacheEntryOptions options = new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(tiempo.Value));
+                this.memoryCache.Set("FECHA", fecha, options);
+                ViewData["MENSAJE"] = "Fecha almacenada correctamente";
+                ViewData["FECHA"] = this.memoryCache.Get("FECHA");
+            }
+            else
+            {
+                fecha = this.memoryCache.Get<string>("FECHA");
+                ViewData["MENSAJE"] = "Fecha recuperada correctamente";
+                ViewData["FECHA"] = fecha;
+            }
+
             return View();
         }
 
