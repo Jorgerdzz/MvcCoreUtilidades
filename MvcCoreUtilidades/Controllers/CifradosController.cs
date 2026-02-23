2c2a915a70ed8dcaf19e6b8a17d803b0f231ad77
Jorgerdzz <asilgalan083@gmail.com>
2026-02-23 11:27:44 +0100

    Criptografia implementada

---
+++ MvcCoreUtilidades/Controllers/CifradosController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreUtilidades.Helpers;
+
+namespace MvcCoreUtilidades.Controllers
+{
+    public class CifradosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CifradoBasico()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CifradoBasico(string contenido, string resultado, string accion)
+        {
+            //CIFRAMOS EL CONTENIDO
+            string response = HelperCryptography.EncriptarTextoBasico(contenido);
+            if(accion.ToLower() == "cifrar")
+            {
+                ViewData["TEXTOCIFRADO"] = response;
+            }else if (accion.ToLower() == "comparar")
+            {
+                if(resultado != response)
+                {
+                    ViewData["MENSAJE"] = "Los datos no coinciden";
+                }
+                else
+                {
+                    ViewData["MENSAJE"] = "Contenidos iguales!!!";
+                }
+            }
+                return View();
+        }
+    }
+}
---
+++ MvcCoreUtilidades/Helpers/HelperCryptography.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MvcCoreUtilidades.Helpers
+{
+    public class HelperCryptography
+    {
+        //CREAMOS LOS METODOS DE TIPO STATIC
+        //SIMPLEMENTE DEVOLVEMOS UN TEXTO CIFRADO SIMPLE
+        public static string EncriptarTextoBasico(string contenido)
+        {
+            //EL CIFRADO SE REALIZA A NIVEL DE BYTES
+            //DEBEMOS CONVERTIR EL TEXTO DE ENTRADA A BYTES[]
+            byte[] entrada;
+            //DESPUES DE CIFRAR LOS BYTES, NOS DARA UNA SALIDA DE BYTES[]
+            byte[] salida;
+            //NECESITAMOS UNA CLASE PARA CONVERTIR DE BYTE[] A STRING Y VICEVERSA
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            //NECESITAMOS UN OBJETO PARA CIFRAR EL CONTENIDO
+            SHA1 managed = SHA1.Create();
+            //CONVERTIMOS EL TEXTO DE ENTRADA A BYTES
+            entrada = encoding.GetBytes(contenido);
+            //LOS OBJETOS DE CIFRADO TIENEN UN METODO LLAMADO
+            //ComputeHash() QUE RECIBEN UN ARRAY DE BYTES, REALIZAN
+            //ACCIONES INTERNAS Y DEVUELVEN EL ARRAY DE BYTES[] CIFRADO
+            salida = managed.ComputeHash(entrada);
+            string resultado = encoding.GetString(salida);
+            return resultado;
+        }
+    }
+}
